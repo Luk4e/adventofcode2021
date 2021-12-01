9dcf61eaeab673bd9976fc89167e75ede6e7bd80
Luk4e <66snok66@gmail.com>
2021-12-01 16:00:31 +0100

    fixes

--- day01.js
+++ day01.js
@@ -2,7 +2,8 @@ fs = require('fs');
 
 try{
     let data = fs.readFileSync('/home/luca/Documents/adventofcode2021/input_file/day01input.txt','utf8');
-    const lines = data.split(/\r?\n/);
+    let lines = data.split(/\r?\n/);
+    
     let count = 0;
     lines.reduce((prev,curr)=>{
         if(parseInt(prev)<parseInt(curr)){
--- day01_2.js
+++ day01_2.js
@@ -3,7 +3,7 @@ fs = require('fs');
 try{
     let data = fs.readFileSync('/home/luca/Documents/adventofcode2021/input_file/day01_2input.txt','utf8');
     const lines = data.split(/\r?\n/);
-    
+
     console.log(countIncrise(lines))
 }catch(err){
     console.log(err);
@@ -14,9 +14,9 @@ try{
 function countIncrise(arr){
     let count = 0;
     for(let i = 0;i<(arr.length-3);i++){
-        let firts = parseInt(arr[i])+parseInt(arr[i+1])+parseInt(arr[i+2])
+        let first = parseInt(arr[i])+parseInt(arr[i+1])+parseInt(arr[i+2])
         let second = parseInt(arr[i+1])+parseInt(arr[i+2])+parseInt(arr[i+3])
-        if(second>firts){
+        if(second>first){
             count++;
         }
     }
