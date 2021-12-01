f1c4e078b7b4c09f6de0bd6c89d5a7bcd84400f6
Luk4e <66snok66@gmail.com>
2021-12-01 14:42:58 +0100

    first day second star

--- day01.js
+++ day01.js
@@ -1,11 +1,7 @@
-const { count } = require('console');
-const { POINT_CONVERSION_UNCOMPRESSED } = require('constants');
-
 fs = require('fs');
-let data;
 
 try{
-    data = fs.readFileSync('/home/luca/Documents/adventofcode2021/file_to_read/day01input.txt','utf8');
+    let data = fs.readFileSync('/home/luca/Documents/adventofcode2021/file_to_read/day01input.txt','utf8');
     const lines = data.split(/\r?\n/);
     let count = 0;
     lines.reduce((prev,curr)=>{
@@ -14,7 +10,7 @@ try{
         }
         return curr
     })
-    
+
     console.log(count)
 }catch(err){
     console.log(err);
